d192f7737fc42a5836752af83f125bd36667402d
Tomáš Nosek <nosek.tomas2004@gmail.com>
2026-04-14 12:56:17 +0200

    lap_ctrl --> add I/O

--- Vivado Project/DE1-Project-Stopwatch_VivadoProject/DE1-Project-Stopwatch_VivadoProject.srcs/sources_1/new/lap_ctrl.vhd
+++ Vivado Project/DE1-Project-Stopwatch_VivadoProject/DE1-Project-Stopwatch_VivadoProject.srcs/sources_1/new/lap_ctrl.vhd
@@ -1,42 +1,37 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 14.04.2026 12:38:08
--- Design Name: 
--- Module Name: lap_ctrl - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity lap_ctrl is
---  Port ( );
+    Port (
+        clk : in STD_LOGIC;
+        rst : in STD_LOGIC;
+        lap_button : in STD_LOGIC;                              -- Button to store actual time to memory
+        time_actual : in STD_LOGIC_VECTOR (31 downto 0);        -- Actual time to store
+        sw : in STD_LOGIC_VECTOR (15 downto 0);                 -- Switches to choose memory slot to display
+        lap_time_output : out STD_LOGIC_VECTOR (31 downto 0);   -- Time from memory to be displayed
+        show_lap :  out STD_LOGIC;                              -- Info to display_ctrl that we want to show time from lap_ctrl
+        led : out STD_LOGIC_VECTOR (15 downto 0)                -- LEDs to indicate
+        
+      );
 end lap_ctrl;
 
 architecture Behavioral of lap_ctrl is
 
+    ----------------------------------------------------------------
+    -- 1. Component declaration for clock enable --> for LED blinking
+    ----------------------------------------------------------------
+    component clk_en is
+        generic ( G_MAX : positive);  
+        Port ( clk : in STD_LOGIC;
+               rst : in STD_LOGIC;
+               ce : out STD_LOGIC);
+    end component clk_en;
+ 
+    ----------------------------------------------------------------
+    -- 2. Signal Declaration
+    ----------------------------------------------------------------
+    signal sig_ce_2hz : STD_LOGIC; -- for LED Blinkg
 begin
 
 
