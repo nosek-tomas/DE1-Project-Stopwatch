d1f3b2cd3f8bec3ba684fe9b0f25acaca06e31aa
Tomáš Nosek <nosek.tomas2004@gmail.com>
2026-04-17 08:25:00 +0200

    Update lap_ctrl.vhd

--- Vivado Project/DE1-Project-Stopwatch_VivadoProject/DE1-Project-Stopwatch_VivadoProject.srcs/sources_1/new/lap_ctrl.vhd
+++ Vivado Project/DE1-Project-Stopwatch_VivadoProject/DE1-Project-Stopwatch_VivadoProject.srcs/sources_1/new/lap_ctrl.vhd
@@ -32,6 +32,7 @@ architecture Behavioral of lap_ctrl is
     -- 2. Signal Declaration
     ----------------------------------------------------------------
     signal sig_ce_2hz : STD_LOGIC;                      -- for LED Blinkg
+    signal sig_blinking : STD_LOGIC;                    -- for LED Blinkg
     signal sig_write_ptr : integer range 0 to 16 := 0;  -- Numeric pointer to first free memory slot to write
     
     ----------------------------------------------------------------
@@ -39,28 +40,45 @@ architecture Behavioral of lap_ctrl is
     ----------------------------------------------------------------
     type t_memory is array (0 to 15) of std_logic_vector(31 downto 0); --new datatype
     signal s_memory : t_memory := (others => (others => '0'));         -- Create storage and initialise with zeros 
+    
 begin
 
     ----------------------------------------------------------------
-    -- Clock Enable instantiation --> for LED blinking
+    -- Clock Enable instantiation --> for LED blinking (2Hz)
     ----------------------------------------------------------------
-    
+    blink_gen : clk_en
+        generic map ( G_MAX => 50_000_000 ) -- 100MHz / 2Hz = 50M
+        port map (
+            clk => clk,
+            rst => rst,
+            ce => sig_ce_2hz
+        );
+        
+    ----------------------------------------------------------------
+    -- Saveing actual time to memory
+    ----------------------------------------------------------------        
     p_memory_write : process (clk)
     begin
         if rising_edge(clk) then
             if rst = '1' then
                 s_memory <= (others => (others => '0')); -- reset memory
-                led <= (others => '0'); -- reset LEDS
-                sig_write_ptr <= 16;    -- move pointer to first (resp. last) memory slot
-            else
-                -- ADD SAVE LOGIC!!!!
+                -- led <= (others => '0'); -- reset LEDS; no needindg for that, state is based on memory allocation
+                sig_write_ptr <= 0;    -- move pointer to first memory slot
+            elsif lap_button = '1' then
+                if sig_write_ptr < 16 then
+                        s_memory(sig_write_ptr) <= time_actual; --save to actually pointed memory slot
+                        sig_write_ptr <= sig_write_ptr + 1;     -- increment pointer to next memory slot
+                    end if;
             end  if;
         end  if;
     end process p_memory_write;
     
-    
+    ----------------------------------------------------------------
+    -- Reading and showing selected lap from memory
+    ----------------------------------------------------------------     
     p_memory_read_and_leds : process (sw, sig_write_ptr, s_memory, sig_ce_2hz)
     begin
+    
     end process p_memory_read_and_leds;
 
 end Behavioral;
