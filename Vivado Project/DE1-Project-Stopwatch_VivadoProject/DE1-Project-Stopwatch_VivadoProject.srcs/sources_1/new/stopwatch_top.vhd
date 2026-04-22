acffdc311fe20f4e39cba253d533c0df29e89538
Tomáš Nosek <nosek.tomas2004@gmail.com>
2026-04-22 18:22:34 +0200

    Implemented lap_ctrl to top

--- Vivado Project/DE1-Project-Stopwatch_VivadoProject/DE1-Project-Stopwatch_VivadoProject.srcs/sources_1/new/stopwatch_top.vhd
+++ Vivado Project/DE1-Project-Stopwatch_VivadoProject/DE1-Project-Stopwatch_VivadoProject.srcs/sources_1/new/stopwatch_top.vhd
@@ -4,13 +4,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity stopwatch_top is
-    Port ( clk : in STD_LOGIC;
+    Port ( clk  : in STD_LOGIC;
            btnd : in STD_LOGIC; -- Reset
            btnl : in STD_LOGIC; -- Start/stop
-           --btnr : in STD_LOGIC;  -- Lap
-           seg : out STD_LOGIC_VECTOR (6 downto 0); -- Segment display
-           an : out STD_LOGIC_VECTOR (7 downto 0); -- Anodes to turn on specific display
-           dp :out STD_LOGIC
+           btnr : in STD_LOGIC;  -- Lap
+           sw   : in STD_LOGIC_VECTOR (15 downto 0);
+           seg  : out STD_LOGIC_VECTOR (6 downto 0); -- Segment display
+           an   : out STD_LOGIC_VECTOR (7 downto 0); -- Anodes to turn on specific display
+           dp   : out STD_LOGIC;
+           led  : out STD_LOGIC_VECTOR(15 downto 0) 
            );
 end stopwatch_top;
 
@@ -79,19 +81,36 @@ architecture Behavioral of stopwatch_top is
         );
     end component;
     
+    component lap_ctrl is
+        Port (
+        clk             : in STD_LOGIC;
+        rst             : in STD_LOGIC;
+        lap_button      : in STD_LOGIC;                       
+        time_actual     : in STD_LOGIC_VECTOR (31 downto 0);  
+        sw              : in STD_LOGIC_VECTOR (15 downto 0);  
+        lap_time_output : out STD_LOGIC_VECTOR (31 downto 0); 
+        show_lap        :  out STD_LOGIC;                     
+        led             : out STD_LOGIC_VECTOR (15 downto 0)                
+        
+      );
+    end component;
     ------------------------------------------------------------------------
     -- 2.  Signal Declarations
     ------------------------------------------------------------------------
     -- Signals for buttons
-     signal sig_btn_toggle_debounced_pulse: std_logic;
+     signal sig_btn_toggle_debounced_pulse: STD_LOGIC;
+     signal sig_btn_lap_debounced_pulse: STD_LOGIC;
     
     -- Signals for time ticks
-    signal sig_ce_100Hz : std_logic;
-    signal sig_ce_100Hz_toggled : std_logic;
+    signal sig_ce_100Hz : STD_LOGIC;
+    signal sig_ce_100Hz_toggled : STD_LOGIC;
     
     -- Signals for time data
-    signal sig_actual_time : STD_LOGIC_VECTOR (31 downto 0);
+    signal sig_actual_time  : STD_LOGIC_VECTOR (31 downto 0);
     signal sig_display_time : STD_LOGIC_VECTOR (31 downto 0);
+    signal sig_lap_time     : STD_LOGIC_VECTOR (31 downto 0);
+    
+    signal sig_show_lap : STD_LOGIC;
     
 begin
     ------------------------------------------------------------------------
@@ -114,6 +133,17 @@ begin
             btn_in     => btnl,
             btn_state  => open, -- Not used
             btn_press  => sig_btn_toggle_debounced_pulse, 
+            btn_relase => open  -- Not used
+        );
+ 
+    -- Debouncer for LAP button
+    debouncer_btn_lap_inst : debounce
+        port map (
+            clk        => clk,
+            rst        => btnd,
+            btn_in     => btnr,
+            btn_state  => open, -- Not used
+            btn_press  => sig_btn_lap_debounced_pulse, 
             btn_relase => open  -- Not used
         );
 
@@ -140,8 +170,8 @@ begin
     display_switch_inst : display_ctrl
         port map (
             data_live => sig_actual_time,
-            data_lap  => x"12345678", --dummy value for first version
-            show_lap  => '0',        -- Dont have LAP implemented yet
+            data_lap  => sig_lap_time,
+            show_lap  => sig_show_lap,
             data_out  => sig_display_time
         );
 
@@ -155,5 +185,17 @@ begin
             anode => an,
             datapoint => dp
         );
-    
+        
+      lap_ctrl_inst : lap_ctrl
+        port map (
+            clk => clk,         
+            rst => btnd,     
+            lap_button => sig_btn_lap_debounced_pulse,
+            time_actual => sig_actual_time,   
+            sw => sw,             
+            lap_time_output => sig_lap_time, 
+            show_lap => sig_show_lap,  
+            led => led
+        );
+ 
 end Behavioral;
